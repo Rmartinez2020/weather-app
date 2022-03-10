6379560c3f1e47b0d28dac994c743e54b76c961b
Rogelio Martinez <rmartinez5926.rm@gmail.com>
2022-03-10 18:30:45 -0500

    ADD home component

---
+++ client/src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Home() {
+  return (
+        <Container>
+            Home component
+        </Container>
+  )
+}
+
+export default Home;
+
+// CSS
+
+const Container = styled.div`
+    height: 100vh;
+`
